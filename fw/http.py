70eb0d5a87c8f2c073ef29836f3d5dafbb193f18
Compeito <com0806peito@icloud.com>
2019-02-28 21:22:39 +0900

    update

--- fw/http.py
+++ fw/http.py
@@ -1,8 +1,9 @@
-def http404(env, start_response):
-    start_response('404 Not Found', [('Content-type', 'text/plain; charset=utf-8')])
-    return [b'404 Not Found']
+from .response import Response
 
 
-def http405(env, start_response):
-    start_response('405 Method Not Allowed', [('Content-type', 'text/plain; charset=utf-8')])
-    return [b'405 Method Not Allowed']
+def http404(request):
+    return Response(status=404)
+
+
+def http405(request):
+    return Response(status=405)
