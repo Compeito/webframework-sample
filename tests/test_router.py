cf2a9ef25c5e2d6b9d93b2a49bfd060e9bac18e3
Compeito <com0806peito@icloud.com>
2019-03-01 19:24:48 +0900

    ユニットテストの試験的な追加

---
+++ tests/test_router.py
@@ -0,0 +1,33 @@
+import re
+import unittest
+
+from fw.response import Response
+from fw.router import Router, route_to_regex
+from fw.http import http404
+
+
+def view(name):
+    return Response(f'hello {name}')
+
+
+class RouterTests(unittest.TestCase):
+
+    def setUp(self):
+        self.router = Router()
+        self.router.add('/hoge/<fuga>', 'GET', view)
+
+    def test_route_to_tegex(self):
+        test_paths = (
+            ('/', '^/$'),
+            ('/hoge/<fuga>', '^/hoge/(?P<fuga>\\w+)$'),
+        )
+        for a, b in test_paths:
+            self.assertEqual(route_to_regex(a), re.compile(b))
+
+    def test_url_vars(self):
+        callback, url_vars = self.router.match('/hoge/foo', 'GET')
+        self.assertEqual(url_vars, {'fuga': 'foo'})
+
+    def test_404_not_found(self):
+        callback, url_vars = self.router.match('/fuga', 'GET')
+        self.assertEqual(callback, http404)
